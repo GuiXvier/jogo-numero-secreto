ee168592fb91f9a8b395ac48f5c957d8bd4882ae
GuiXvier <guilherme.contatogg@gmail.com>
2023-06-07 10:03:35 -0300

    Aparece no console o que foi falado pelo usuario

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -3,3 +3,9 @@ window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-br'
 recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+    console.log(e.results[0][0].transcript);
+}
