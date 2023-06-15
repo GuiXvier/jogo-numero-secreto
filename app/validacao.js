3e77bd0271b510a3ff32b4e2e0a5161cc69212b0
GuiXvier <guilherme.contatogg@gmail.com>
2023-06-15 17:18:16 -0300

    Jogo funcional

--- app/validacao.js
+++ app/validacao.js
@@ -3,17 +3,25 @@ function verificaSeOchutePossuiUmValorValido(chute){
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
     }
 
     if (numeroForMaiorOuMenorQueValorPermitdo(numero)){
         elementoChute.innerHTML += `<div>valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
     }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você acertou</h2>
             <h3>O numero secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente class="btn-jogar">Jogar novamente</button>
         `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O numero secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O numero secreto é maior <i class="fa-solid fa-up-long"></i></div>`
     }
 }
 
@@ -24,3 +32,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueValorPermitdo(numero){
     return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+})
